a94665f16487afbd11f4f78a58007d21c988482c
sanghun oh <sanghunoh@sanghuns-MacBook-Air.local>
2020-08-30 11:29:39 +0900

    rename folder

--- OpenCV_py/readingfromVideoFile.py
+++ OpenCV_py/readingfromVideoFile.py
@@ -2,6 +2,7 @@ import cv2
 
 # videoFile = 'videos/Armbot.mp4'
 videoFile = 'videos/output.avi'
+videoFile = 'OpenCV_py/videos/output.avi'
 cap = cv2.VideoCapture(videoFile)
 while not cap.isOpened():
     cap = cv2.VideoCapture(videoFile)
