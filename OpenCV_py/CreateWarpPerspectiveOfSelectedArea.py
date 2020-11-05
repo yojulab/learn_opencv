ec0838863a465fb501d5e69cc2da06a63ba2dd52
Jane Vallette <janevallette@Janes-MacBook-Air.local>
2020-11-05 21:58:18 +0900

    add some files

--- OpenCV_py/CreateWarpPerspectiveOfSelectedArea.py
+++ OpenCV_py/CreateWarpPerspectiveOfSelectedArea.py
@@ -4,7 +4,8 @@ import numpy as np
 img = cv.imread("datas/images/cards.jpg")
 
 width,height = 250,350
-pts1 = np.float32([[111,219],[287,188],[154,482],[352,440]])
+pts1 = np.float32([[111,219],[287,188],[154,482],[352,440]])  # bottom card
+# pts1 = np.float32([[280,120],[450,130],[280,340],[454,369]])    # right card
 pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
 matrix = cv.getPerspectiveTransform(pts1,pts2)
 imgOutput = cv.warpPerspective(img,matrix,(width,height))
@@ -12,6 +13,10 @@ imgOutput = cv.warpPerspective(img,matrix,(width,height))
 
 cv.imshow("Image",img)
 cv.imshow("Output",imgOutput)
+
+import matplotlib.pyplot as plt
+plt.imshow(img)
+plt.show()
 
 cv.waitKey(0)
 cv.destroyAllWindows()
