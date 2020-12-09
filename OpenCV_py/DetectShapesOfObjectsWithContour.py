e15e543b2939e5c6fc2474d94da8cfb471ebab75
SanghunOh <otter.oh@gmail.com>
2020-12-09 15:32:46 +0900

    change some files

--- OpenCV_py/DetectShapesOfObjectsWithContour.py
+++ OpenCV_py/DetectShapesOfObjectsWithContour.py
@@ -57,23 +57,25 @@ def getContours(img):
             objCor = len(approx)
             x, y, w, h = cv.boundingRect(approx)
 
+            objectType = None
             if objCor == 3:
                 objectType = "Tri"
-            elif objCor == 4:
-                aspRatio = w/float(h)
-                if aspRatio > 0.98 and aspRatio < 1.03:
-                    objectType = "Square"
-                else:
-                    objectType = "Rectangle"
-            elif objCor > 4:
-                objectType = "Circles"
-            else:
-                objectType = "None"
+            # elif objCor == 4:
+            #     aspRatio = w/float(h)
+            #     if aspRatio > 0.98 and aspRatio < 1.03:
+            #         objectType = "Square"
+            #     else:
+            #         objectType = "Rectangle"
+            # elif objCor > 4:
+            #     objectType = "Circles"
+            # else:
+            #     objectType = "None"
 
-            cv.rectangle(imgContour, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            cv.putText(imgContour, objectType,
-                       (x+(w//2)-10, y+(h//2)-10), cv.FONT_HERSHEY_COMPLEX, 0.7,
-                       (0, 0, 0), 2)
+            if objectType:
+                cv.rectangle(imgContour, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                cv.putText(imgContour, objectType,
+                        (x+(w//2)-10, y+(h//2)-10), cv.FONT_HERSHEY_COMPLEX, 0.7,
+                        (0, 0, 0), 2)
 
 
 path = 'datas/images/shapes.png'
