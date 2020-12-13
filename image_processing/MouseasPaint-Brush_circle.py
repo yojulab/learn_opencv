7204c667502b44f594939d767b62efdb14e3fd8b
sanghun oh <sanghunoh@sanghuns-MacBook-Air.local>
2020-12-13 15:40:19 +0900

    change some files

--- image_processing/MouseasPaint-Brush_circle.py
+++ image_processing/MouseasPaint-Brush_circle.py
@@ -2,17 +2,17 @@ from cv2 import cv2 as cv
 import numpy as np
 
 # mouse callback function
-def draw_circle(event,x,y,flags,param):
+def draw_shape(event,x,y,flags,param):
     if event == cv.EVENT_FLAG_LBUTTON:
         cv.circle(img,(x,y),10,(255,0,0),-1)
 
 # Create a black image, a window and bind the function to window
 img = np.zeros((512,512,3), np.uint8)
 cv.namedWindow('image')
-cv.setMouseCallback('image',draw_circle)
+cv.setMouseCallback('image',draw_shape)
 
 while(1):
     cv.imshow('image',img)
-    if cv.waitKey(20) & 0xFF == 27:
+    if cv.waitKey(20) & 0xFF == ord('q'):
         break
 cv.destroyAllWindows()
--- image_processing/MouseasPaint-Brush_rectangle.py
+++ image_processing/MouseasPaint-Brush_rectangle.py
@@ -2,12 +2,11 @@ from cv2 import cv2 as cv
 import numpy as np
 
 drawing = False  # true if mouse is pressed
-mode = True  # if True, draw rectangle. Press 'm' to toggle to curve
 ix, iy = -1, -1
 
 # mouse callback function
 def draw_shape(event, x, y, flags, param):
-    global ix, iy, drawing, mode
+    global ix, iy, drawing
 
     if event == cv.EVENT_LBUTTONDOWN:
         drawing = True
@@ -15,17 +14,11 @@ def draw_shape(event, x, y, flags, param):
 
     elif event == cv.EVENT_MOUSEMOVE:
         if drawing == True:
-            if mode == True:
-                cv.rectangle(img, (ix, iy), (x, y), (0, 255, 0), 0)
-            else:
-                cv.circle(img, (x, y), 5, (0, 0, 255), 0)
+            cv.rectangle(img, (ix, iy), (x, y), (0, 255, 0), 0)
 
     elif event == cv.EVENT_LBUTTONUP:
         drawing = False
-        if mode == True:
-            cv.rectangle(img, (ix, iy), (x, y), (0, 255, 0), -1)
-        else:
-            cv.circle(img, (x, y), 5, (0, 0, 255), -1)
+        cv.rectangle(img, (ix, iy), (x, y), (0, 255, 0), -1)
 
 
 img = np.zeros((512, 512, 3), np.uint8)
@@ -34,10 +27,7 @@ cv.setMouseCallback('image', draw_shape)
 
 while(1):
     cv.imshow('image', img)
-    k = cv.waitKey(1) & 0xFF
-    if k == ord('m'):
-        mode = not mode
-    elif k == ord('q'):
+    if cv.waitKey(1) == ord('q'):
         break
 
 cv.destroyAllWindows()
