be6c5b9812a8ec9f3d8a0029f55fa28b58c9e2d8
Sanghun Oh <otter.oh@gmail.com>
2021-01-06 21:59:32 +0900

    change some file

---
+++ OpenCV_py/CreateDataset.py
@@ -0,0 +1,31 @@
+import os
+from cv2 import cv2 as cv
+import numpy as np
+
+def create_dataset(img_folder):
+    img_data_array = []
+    class_name = []
+
+    IMG_WIDTH = 200
+    IMG_HEIGHT = 200
+    for dir1 in os.listdir(img_folder):
+        for file in os.listdir(os.path.join(img_folder, dir1)):
+            image_path = os.path.join(img_folder, dir1,  file)
+            image = cv.imread(image_path, cv.COLOR_BGR2RGB)
+            image = cv.resize(image, (IMG_HEIGHT, IMG_WIDTH),
+                        interpolation=cv.INTER_AREA)
+            image = np.array(image)
+            image = image.astype('float32')
+            image /= 255
+            img_data_array.append(image)
+            class_name.append(dir1)
+
+    return (img_data_array, class_name)
+
+# extract the image array and class name
+img_data, class_name = create_dataset(r'CV\Intel_Images\seg_train\seg_train')
+
+print(class_name)
+
+cv.imshow('image', img_data[3])
+cv.waitKey(0)
