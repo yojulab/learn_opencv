6f743fe85e8dde88148c417a27ec8a27f835e57f
Sanghun Oh <otter.oh@gmail.com>
2021-01-17 18:41:18 +0900

    change some files

--- OpenCV_py/ReadFromUDPProtocol.py
+++ OpenCV_py/ReadFromUDPProtocol.py
@@ -2,9 +2,9 @@ from cv2 import cv2 as cv
 
 # Test Previous(Singlecast, Not Multicast)
 # Server (ip : 192.168.0.12), Client (ip : 192.168.0.6)
-# ~$ ffmpeg -v verbose -f v4l2 -i /dev/video0 -s 100x70 -f mpegts udp://192.168.0.6:20001		# Server to Client
-# ~$ ffplay udp://127.0.0.1:20001
-addr = 'udp://:20001'
+# ~$ ffmpeg -v verbose -f v4l2 -i /dev/video0 -s 100x70 -f mpegts udp://192.168.0.6:1234		# Server to Client
+# ~$ ffplay udp://127.0.0.1:1234
+addr = 'udp://:1234'
 cap = cv.VideoCapture(addr, cv.CAP_FFMPEG)
 if not cap.isOpened():
     print('VideoCapture not opened')
