ffe45d87fc1c97bc3caf002a733af48701e73a8d
SanghunOh <otter.oh@gmail.com>
2021-09-14 15:08:18 +0900

    detector QR code

--- detection/15 QR codes detector and recognizer.py
+++ detection/15 QR codes detector and recognizer.py
@@ -44,46 +44,53 @@ def qr_code_outer_corners(image):
     
     # cv2.imshow('image',image)
     # cv2.imshow('gray',gray)
-    # cv2.imshow('th',th)
+    cv2.imshow('th',th)
+    cv2.waitKey()
 
     # 찾을 물체는 흰색이어야하고 배경은 검은색 
     contours, hierarchy = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    ret = max(contours, key=len)
-    draw_contour = cv2.drawContours(gray, contours, -1, (0,255,0), 2)
+    # ret = max(contours, key=len)
+    # draw_contour = cv2.drawContours(gray, contours, -1, (0,255,0), 2)
     # draw_contour = cv2.drawContours(gray, [ret], 0, (0,255,0),2)
-    cv2.imshow('draw_contour', draw_contour)
-    cv2.waitKey()
+    for contour in contours:
+        draw_contour = cv2.drawContours(gray, [contour], 0, (255,0,0), 2)
+        cv2.imshow('draw_contour', draw_contour)
+        cv2.waitKey()
     cv2.destroyAllWindows()
     
     cnts = []
     centers = []
+    corners_point = list()
         
     hierarchy = hierarchy.reshape((-1, 4))
     for i in range(hierarchy.shape[0]):
         i_next, i_prev, i_child, i_par = hierarchy[i]
-        if all(v == -1 for v in hierarchy[i][:3]):
-            if all(v == -1 for v in hierarchy[i_par][:2]):
-                ids = [i, i_par, hierarchy[i_par][3]]
-                corner_cnts = []
-                for id_ in ids:
-                    cnt = contours[id_]
-                    apprx = cv2.approxPolyDP(cnt, cv2.arcLength(cnt, True) * 0.02, True)
-                    if len(apprx) == 4:
-                        corner_cnts.append(apprx.reshape((4, -1)))
-                if len(corner_cnts) == 3:
-                    cnts.append(corner_cnts)
-                    all_pts = np.array(corner_cnts).reshape(-1, 2)
-                    
-                    centers.append(np.mean(all_pts, 0))
+        if all(v == -1 for v in hierarchy[i][:3]) & all(v == -1 for v in hierarchy[i_par][:2]):
+            ids = [i, i_par, hierarchy[i_par][3]]
+            corner_cnts = []
+            for id_ in ids:
+                cnt = contours[id_]
+                apprx = cv2.approxPolyDP(cnt, cv2.arcLength(cnt, True) * 0.02, True)
+                if len(apprx) == 4:
+                    corner_cnts.append(apprx.reshape((4, -1)))
+            if len(corner_cnts) == 3:
+                cnts.append(corner_cnts)
+                all_pts = np.array(corner_cnts).reshape(-1, 2)
+                centers.append(np.mean(all_pts, 0))
     
-    if len(centers) == 3:        
-        distances_between_pts = np.linalg.norm(np.roll(centers, 1, 0) - centers, axis=1)
+    # to deside top-right, top-left, bottom-left point
+    if len(centers) == 3:
+        centers_norm = [np.linalg.norm(center) for center in centers]
+        top_right_index = np.argmax(centers_norm)
+
+        distances_between_pts = np.linalg.norm(np.roll(centers, shift=1, axis=0) - centers, axis=1)
         max_dist_id = np.argmax(distances_between_pts)
         
-        index_diag_pt_1 = max_dist_id
-        index_diag_pt_2 = (max_dist_id - 1) % len(centers)
-        index_corner_pt = (len(centers) - 1)*len(centers) // 2 - index_diag_pt_1 - index_diag_pt_2
-        
+        index_diag_pt_1 = max_dist_id       # top-right index
+        index_diag_pt_2 = (max_dist_id - 1) % len(centers)  # bottom-left index
+        index_corner_pt = (len(centers) - 1)*len(centers) // 2 - index_diag_pt_1 - index_diag_pt_2  # top
+        # index_corner_pt = [item for item in range(3) if item not in [index_diag_pt_1, index_diag_pt_2]][0]
+
         middle_pt = 0.5 * (centers[index_diag_pt_1] + centers[index_diag_pt_2])
         
         i_ul_pt = np.argmax(np.linalg.norm(cnts[index_corner_pt][-1] - middle_pt, axis=1))
@@ -91,7 +98,7 @@ def qr_code_outer_corners(image):
                 
         for i in [index_diag_pt_1, index_diag_pt_2]:
             corner_cnts = cnts[i]
-            outer_cnt = corner_cnts[-1]
+            outer_cnt = corner_cnts[-1]     # outer line points
             
             distances_to_mp = np.linalg.norm(outer_cnt - middle_pt, axis=1)
             max_dist_id = np.argmax(distances_to_mp)      
@@ -118,9 +125,11 @@ def qr_code_outer_corners(image):
     return outer_corners_found, outer_corners
 
 # read image
-frame = cv2.imread('./datas/images/qr_01.png') # can detect marker
-# frame = cv2.imread('../data/qr_02.png') # can't detect marker
+# frame = cv2.imread('./datas/images/qr_01.png') # can detect marker
+frame = cv2.imread('./datas/images/qr_02.png') # can't detect marker
 if frame is not None:
+    # frame = cv2.flip(frame, 1)
+    # cv2.imshow('flip', frame)
     result, corners = qr_code_outer_corners(frame)
 
     qr_code_size = 300
