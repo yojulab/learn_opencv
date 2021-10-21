9f16f0f1a068eb07b990c89f3413dd64d4785373
Sanghun Oh <otter.oh@gmail.com>
2021-10-21 18:05:56 +0900

    change normal code

--- basic/camera.py
+++ basic/camera.py
@@ -2,9 +2,9 @@ import cv2 as cv
 #import numpy
 
 cam = cv.VideoCapture(0)
-cam.set(cv.CAP_PROP_FRAME_WIDTH, 640)
-cam.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
-cam.set(cv.CAP_PROP_FPS, 7)
+# cam.set(cv.CAP_PROP_FRAME_WIDTH, 640)
+# cam.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
+# cam.set(cv.CAP_PROP_FPS, 7)
 #cv2mat = numpy.array([])
 
 while True:
